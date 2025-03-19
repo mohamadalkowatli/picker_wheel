48534274159989cf845423174ff2c14da2bd71fc
mohmmadalkowatli <mohmmadalkowatli2@gmail.com>
2025-03-20 01:42:45 +0400

    update new layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3765304930"
+    serviceWorkerVersion: "4086115610"
   }
 });
